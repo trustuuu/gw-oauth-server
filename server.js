3933155a6deb6047dd916dde98817d1b869b85e6
James Chang <james.chang@igoodworks.com>
2025-02-21 19:20:10 -0800

    change oauth-server build directory

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ dotenv.config(); //{ path: ".env.development" });
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const oauth_server_path = path.join(__dirname, "./oauth-server/dist");
+const oauth_server_path = path.join(__dirname, "./oauth-server/public");
 
 const app = express();
 
